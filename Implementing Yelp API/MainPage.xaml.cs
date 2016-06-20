5b84a884c7d3bdaf26c566867ce52228d688e60c
theblueprint415 <justin.chizer@gmail.com>
2016-06-20 15:56:22 -0700

    first

---
+++ Implementing Yelp API/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Net;
+using System.Text;
+using CommandLine;
+using Newtonsoft.Json.Linq;
+using SimpleOAuth;
+
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Implementing_Yelp_API
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+    var uriBuilder = new UriBuilder(baseURL);
+    uriBuilder.Query = query.ToString();
+
+            var request = WebRequest.Create(uriBuilder.ToString());
+    request.Method = "GET";
+
+            request.SignRequest(
+                new Tokens {
+                    ConsumerKey = CONSUMER_KEY,
+                    ConsumerSecret = CONSUMER_SECRET,
+                    AccessToken = TOKEN,
+                    AccessTokenSecret = TOKEN_SECRET
+}
+            ).WithEncryption(EncryptionMethod.HMACSHA1).InHeader();
+
+HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+var stream = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+            return JObject.Parse(stream.ReadToEnd());
+}
